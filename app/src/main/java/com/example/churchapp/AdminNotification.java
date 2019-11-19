000ddfc5c93a733888ff66d9070927ad7c0de05c
Muhammad Hassam <hassam86.hy@gmail.com>
2019-11-20 00:25:26 +0500

    admin notification is done

--- app/src/main/java/com/example/churchapp/AdminNotification.java
+++ app/src/main/java/com/example/churchapp/AdminNotification.java
@@ -2,13 +2,60 @@ package com.example.churchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class AdminNotification extends AppCompatActivity {
 
+    DbHandler mydb;
+    SQLiteDatabase db ;
+    Button broadcastNoti;
+    EditText commentNoti;
+    String formattedDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_notification);
+
+        mydb = new DbHandler(this);
+        db = mydb.getReadableDatabase();
+
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        formattedDate = df.format(c);
+
+        commentNoti=(EditText) findViewById(R.id.commentNoti);
+        broadcastNoti=(Button) findViewById(R.id.sendNotification);
+
+        broadcastNoti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+
+                        mydb.insertAdminNotification("admin",commentNoti.getText().toString(),formattedDate,db);
+                        finish();
+                        Toast.makeText(getApplicationContext(),"Notification is broadcast",Toast.LENGTH_SHORT).show();
+                }
+                catch(Exception e){
+                    Toast.makeText(getApplicationContext(),"Enter Valid Credentials",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+
     }
 }
